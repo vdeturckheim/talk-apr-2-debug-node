d4b1fc1dbf72b4ddd6b1db5b57d5ecc24eea82f3
vdeturckheim <vlad2t@hotmail.com>
2020-03-25 16:34:38 +0100

    add 4 main cases

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const app = require('express')();
 app.get('/mem/literal', require('./lib/memleak-literal'));
 app.get('/mem/class', require('./lib/memleak-class'));
 app.get('/cpu/:str', require('./lib/redos'));
-app.get('/async', require('./lib/async'))
+app.get('/async', require('./lib/async'));
 
 module.exports = app;
