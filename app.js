a5faaf79ed4718c18c80cd63f72a1eff3511e6ae
vdeturckheim <vlad2t@hotmail.com>
2020-03-25 16:44:39 +0100

    add dummy endpoint

--- app.js
+++ app.js
@@ -6,4 +6,9 @@ app.get('/mem/class', require('./lib/memleak-class'));
 app.get('/cpu/:str', require('./lib/redos'));
 app.get('/async', require('./lib/async'));
 
+app.get('/', (req, res) => {
+
+    res.end('ok\n');
+});
+
 module.exports = app;
